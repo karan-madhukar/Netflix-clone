10c3fec143ae2adc001a31c38f2cc2e53d9075b3
karan-madhukar <karanmadhukarbunty@gmail.com>
2024-01-07 01:55:51 +0530

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-    <h3 className="text-white">hello</h3>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
     </>
   );
 }
---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+// Api service, www.themoviedb.org
+const key = "6808c4d192784220b9862f14653fdbec";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page2`,
+  requestHorror: `https://api.themoviedb.org/3/movie?api_key=${key}&language=en-US&page1&query=horror`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page1`,
+};
+
+export default requests;
